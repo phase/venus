d8c34ad0407c7022b6c5f251c90bf0668c3e3b5b
Jadon Fowler <j@jadon.io>
2019-08-15 02:17:10 -0700

    VGA & Tests
    Signed-off-by: Jadon Fowler <j@jadon.io>

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,73 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[macro_use]
+extern crate lazy_static;
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Venus";
+mod vga;
+mod serial;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    println!("He{}o", 11);
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    #[cfg(test)] test_main();
 
     loop {}
 }
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests.", tests.len());
+    for test in tests {
+        test();
+    }
+    serial_println!("Tests succeeded!");
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        // UNSAFE: make sure this matches up with the test-args in Cargo.toml
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
